caf61ecf835f279a05591de68443338e2f8e39b6
Rich Lively <richielively@gmail.com>
2021-10-01 22:20:26 -0400

    Only create backup folder if needed

--- b0x/install.sh
+++ b0x/install.sh
@@ -4,14 +4,14 @@ git clone --bare git@github.com:richlively/b0x.git $HOME/.b0x
 function b0x {
 	/usr/bin/git --git-dir=$HOME/.b0x/ --work-tree=$HOME $@
 }
-mkdir -p .b0x-backup
 b0x checkout
 if [ $? = 0 ]; then
 	echo "Checked out b0x.";
 else
 	echo "Backing up pre-existing dot files.";
+	mkdir -p .b0x-backup
 	b0x checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .b0x-backup/{}
+	b0x checkout
 fi;
-b0x checkout
 b0x config status.showUntrackedFiles no
 popd

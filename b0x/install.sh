1bb43e415241f93f88797c1fca32052800eaece0
Richard Lively <richielively@gmail.com>
2021-10-01 21:46:22 -0400

    Create install.sh

---
+++ b0x/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+pushd ~
+git clone --bare git@github.com:richlively/b0x.git $HOME/.b0x
+function b0x {
+	/usr/bin/git --git-dir=$HOME/.b0x/ --work-tree=$HOME $@
+}
+mkdir -p .b0x-backup
+b0x checkout
+if [ $? = 0 ]; then
+	echo "Checked out b0x.";
+else
+	echo "Backing up pre-existing dot files.";
+	b0x checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .b0x-backup/{}
+fi;
+b0x checkout
+b0x config status.showUntrackedFiles no
+popd

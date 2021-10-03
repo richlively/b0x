5809662e70cdcf7b961f6dd59ac83841602d5c58
Rich Lively <richielively@gmail.com>
2021-10-02 23:10:09 -0400

    Add Gradle to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,7 @@
 abbr vi 'vim'
 abbr ni 'nvim'
 abbr b0x 'git --git-dir=$HOME/.b0x --work-tree=$HOME'
+# Gradle
+set -gx GRADLE_USER_HOME "$HOME/.gradle"
+set -gx GRADLE_HOME "$GRADLE_USER_HOME/wrapper/dists/gradle-4.6"
+fish_add_path $GRADLE_HOME/bin

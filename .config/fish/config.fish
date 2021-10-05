6532864408911c023d1af1b4258f992fd4243542
Rich Lively <richielively@gmail.com>
2021-10-04 23:02:00 -0400

    Track colors sepearte from fish_variables (since it is autogenerated) Use more consistent spacing for prompt (postfix has no space)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,12 @@ abbr vi 'vim'
 abbr ni 'nvim'
 abbr b0x 'git --git-dir=$HOME/.b0x --work-tree=$HOME'
 
-contains $HOME/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/bin
+fish_add_path $HOME/bin
 
 # Gradle
 set -gx GRADLE_USER_HOME "$HOME/.gradle"
 set -gx GRADLE_HOME "$GRADLE_USER_HOME/wrapper/dists/gradle-4.6"
-contains $GRADLE_HOME/bin $fish_user_paths; or set -Ua fish_user_paths $GRADLE_HOME/bin
+fish_add_path $GRADLE_HOME/bin
 
 # Oh My Posh
 oh-my-posh --init --shell fish --config ~/.config/b0x.omp.json | source

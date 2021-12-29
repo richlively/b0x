b084e0cbe800d3f8f60d64a27bf5999a78d21e6b
Richard Lively <richielively@gmail.com>
2021-12-28 22:24:07 -0500

    Make the install script more bash friendly

--- b0x/install.sh
+++ b0x/install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-pushd ~
-git clone --bare git@github.com:richlively/b0x.git $HOME/.b0x
+pushd ~ || exit
+git clone --bare git@github.com:richlively/b0x.git "$HOME"/.b0x
 function b0x {
-	/usr/bin/git --git-dir=$HOME/.b0x/ --work-tree=$HOME $@
+	/usr/bin/git --git-dir="$HOME"/.b0x/ --work-tree="$HOME" "$@"
 }
-b0x checkout
-if [ $? = 0 ]; then
+if b0x checkout; then
 	echo "Checked out b0x.";
 else
 	echo "Backing up pre-existing dot files.";
 	mkdir -p .b0x-backup
-	b0x checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .b0x-backup/{}
+	b0x checkout 2>&1 | grep -e "\s+\." | awk "{'print $1'}" | xargs -I{} mv {} .b0x-backup/{}
 	b0x checkout
 fi;
 b0x config status.showUntrackedFiles no
-popd
+popd || exit
